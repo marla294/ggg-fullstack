792184635e9ffe3d5cc805e04b133faddd7fdfc2
marla294@gmail.com <marla294@gmail.com>
2023-10-21 07:32:07 -0500

    Setting up mongodb

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import { SessionProvider } from "next-auth/react"
-import "./styles.css"
+import { SessionProvider } from "next-auth/react";
+import "./styles.css";
 
-import type { AppProps } from "next/app"
-import type { Session } from "next-auth"
+import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
@@ -10,9 +10,10 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
+  console.log({ session });
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
 }
